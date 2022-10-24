8695ef0916dc23252b0feba27d79b287b6b31f80
Hecig <me@ceigh.com>
2022-10-24 15:43:53 +0500

    fix(cmp-nvim-lsp): adapt breaking change

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,8 +62,7 @@ local function on_attach()
 end
 
 -- Configurations
-local capabilities = require("cmp_nvim_lsp")
-    .update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 
 lspconfig.volar.setup {
