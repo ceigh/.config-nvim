a4aca88b8ae18db71c7acc2bbfe9239474c62192
Артем Лебзак <giceh@ya.ru>
2023-12-24 17:36:04 +0300

    fix(context_commentstring): remove from config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,12 +31,4 @@ require("nvim-treesitter.configs").setup {
   highlight = {
     enable = true,
   },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-    -- config = {
-    --   css = '// %s',
-    --   scss = '// %s',
-    -- },
-  },
 }
