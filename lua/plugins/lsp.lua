8d2d040864e738d2494e929559e9686b9d361964
Артем Лебзак <giceh@ya.ru>
2024-05-22 05:33:30 +0500

    chore(lsp): update lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -104,20 +104,27 @@ return {
       },
     }
 
-    lspconfig.lua_ls.setup {
+    require 'lspconfig'.lua_ls.setup {
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. '/.luarc.json') or
+            vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
+
+        client.config.settings.Lua =
+            vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = { version = 'LuaJIT' },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = { vim.env.VIMRUNTIME }
+              }
+            })
+      end,
       settings = {
-        Lua = {
-          runtime = { version = "LuaJIT" },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
-          telemetry = { enable = false },
-        },
-      },
+        Lua = {}
+      }
     }
 
     -- lspconfig.unocss.setup {}
