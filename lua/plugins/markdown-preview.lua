fe4bc3f3c07f936464971e0828a81e760fe41ac5
Артем Лебзак <giceh@ya.ru>
2024-06-19 16:49:28 +0500

    feat(plugins): add md preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,6 @@
+return {
+  "https://github.com/iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+}
