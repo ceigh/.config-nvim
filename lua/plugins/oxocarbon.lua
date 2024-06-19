a9462eeb24c1cec16b0c9928c319ce921f36cbeb
Артем Лебзак <giceh@ya.ru>
2024-06-19 16:45:06 +0500

    refactor(plugins): remove oxocarbon

--- lua/plugins/oxocarbon.lua
+++
@@ -1,7 +0,0 @@
-return {
-  enabled = false,
-  "https://github.com/nyoom-engineering/oxocarbon.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function() vim.cmd("colorscheme oxocarbon") end
-}
