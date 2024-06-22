a674a07dd7bcedf28147d13dba6c9ab0cd1ddeda
Артем Лебзак <giceh@ya.ru>
2024-06-23 00:32:42 +0500

    chore(keys): ignore q

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,6 +6,7 @@ map_key("<up>", "<nop>", "")
 map_key("<down>", "<nop>", "")
 map_key("<left>", "<nop>", "")
 map_key("<right>", "<nop>", "")
+map_key("q", "<nop>", "")
 
 map_key("gc", "<nop>", "")
 map_key("gcc", "<nop>", "")
