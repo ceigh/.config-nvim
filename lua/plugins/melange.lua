c28891f10b8aac5f00229899ed99aa24065b0b10
Артем Лебзак <giceh@ya.ru>
2024-06-23 00:32:22 +0500

    chore(colorscheme): change Search hl

--- lua/plugins/melange.lua
+++ lua/plugins/melange.lua
@@ -2,5 +2,12 @@ return {
   "https://github.com/savq/melange-nvim",
   lazy = false,
   priority = 1000,
-  config = function() vim.cmd("colorscheme melange") end
+  config = function()
+    vim.cmd("colorscheme melange")
+
+    local hl = vim.api.nvim_set_hl
+    hl(0, "Search", { bg = "#fae55c", fg = "#000000" })
+    hl(0, "CurSearch", { link = "Search" })
+    hl(0, "IncSearch", { link = "Search" })
+  end
 }
