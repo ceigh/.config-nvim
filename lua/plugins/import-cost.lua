3ef880cd678ecb399cbd7b8653306d1bc2d95493
Артем Лебзак <giceh@ya.ru>
2024-06-26 19:56:10 +0500

    feat(plugins): add import-cost

---
+++ lua/plugins/import-cost.lua
@@ -0,0 +1,17 @@
+local ft = {
+  "javascript",
+  "typescript",
+}
+
+return {
+  "https://github.com/barrett-ruth/import-cost.nvim",
+  build = 'sh install.sh bun install',
+  ft = ft,
+
+  config = {
+    filetypes = ft,
+    format = {
+      virtual_text = "%s, gz: %s",
+    },
+  },
+}
